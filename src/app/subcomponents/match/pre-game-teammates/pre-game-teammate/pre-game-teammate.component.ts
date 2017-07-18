0db6311befcf8dc152318bbecbaf49392f57864f
Mew-www <mew.proxy@hotmail.com>
2017-07-18 16:05:29 +0300

    Add error handler for pre-game-teammate (match history request).

--- src/app/subcomponents/match/pre-game-teammates/pre-game-teammate/pre-game-teammate.component.ts
+++ src/app/subcomponents/match/pre-game-teammates/pre-game-teammate/pre-game-teammate.component.ts
@@ -7,6 +7,8 @@ import {GameType} from "../../../../enums/game-type";
 import {GameReference} from "../../../../models/dto/game-reference";
 import {GameApiService} from "../../../../services/game-api.service";
 import {GameMetadataService} from "../../../../services/game-metadata.service";
+import {Subscription} from "rxjs/Subscription";
+import {ChampionsContainer} from "../../../../models/dto/containers/champions-container";
 
 @Component({
   selector: 'pre-game-teammate',
@@ -25,7 +27,8 @@ export class PreGameTeammateComponent implements OnInit {
   @Output() selectedInitialRole: EventEmitter<boolean> = new EventEmitter();
 
   private role = null;
-  private errors = [];
+  private error_message = '';
+  private subscription: Subscription = null;
   private readonly time_limit_days = 21;
   private soloqueue_games_this_season = null;
   private flexqueue_games_this_season = null;
@@ -33,6 +36,9 @@ export class PreGameTeammateComponent implements OnInit {
   private flexqueue_games_past_3_weeks = null;
   private current_queue_past_3_weeks = null;
   private loading_ready: boolean = false;
+
+  // Metadata
+  private champions: ChampionsContainer;
 
   // Utils
   private GameType = GameType;
@@ -53,48 +59,55 @@ export class PreGameTeammateComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    this.metadata.champions$
-      .first()
-      .subscribe(champions_container => {
-        this.buffered_requests.buffer(() => {
-          return this.player_api.getListOfRankedGamesJson(this.summoner.region, this.summoner.account_id, GameType.SOLO_AND_FLEXQUEUE_5V5, champions_container)
-        })
-          .subscribe(api_res => {
-            if (api_res.type === ResType.SUCCESS) {
-              let all_games = <Array<GameReference>>api_res.data;
-              let soloqueue_games_this_season = all_games.filter(gameref => gameref.game_type === GameType.SOLO_QUEUE);
-              let flexqueue_games_this_season = all_games.filter(gameref => gameref.game_type === GameType.FLEX_QUEUE_5V5);
-              let soloqueue_games_past_3_weeks = soloqueue_games_this_season
-                .filter(gameref => gameref.game_start_time.getTime() > (new Date().getTime()-1000*60*60*24*this.time_limit_days));
-              let flexqueue_games_past_3_weeks = flexqueue_games_this_season
-                .filter(gameref => gameref.game_start_time.getTime() > (new Date().getTime()-1000*60*60*24*this.time_limit_days));
+  private queryMatchHistory() {
+    if (this.subscription && !this.subscription.closed) {
+      return; // Request in progress, noop
+    }
 
-              if (soloqueue_games_this_season.length > 0) {
-                this.soloqueue_games_this_season = soloqueue_games_this_season;
-              }
-              if (flexqueue_games_this_season.length > 0) {
-                this.flexqueue_games_this_season = flexqueue_games_this_season;
-              }
-              if (soloqueue_games_past_3_weeks.length > 0) {
-                this.soloqueue_games_past_3_weeks = soloqueue_games_past_3_weeks;
-              }
-              if (flexqueue_games_past_3_weeks.length > 0) {
-                this.flexqueue_games_past_3_weeks = flexqueue_games_past_3_weeks;
-              }
+    this.subscription = this.buffered_requests.buffer(() => {
+      return this.player_api.getListOfRankedGamesJson(this.summoner.region, this.summoner.account_id, GameType.SOLO_AND_FLEXQUEUE_5V5, this.champions)
+    })
+      .subscribe(api_res => {
+        if (api_res.type === ResType.SUCCESS) {
+          let all_games = <Array<GameReference>>api_res.data;
+          let soloqueue_games_this_season = all_games.filter(gameref => gameref.game_type === GameType.SOLO_QUEUE);
+          let flexqueue_games_this_season = all_games.filter(gameref => gameref.game_type === GameType.FLEX_QUEUE_5V5);
+          let soloqueue_games_past_3_weeks = soloqueue_games_this_season
+            .filter(gameref => gameref.game_start_time.getTime() > (new Date().getTime()-1000*60*60*24*this.time_limit_days));
+          let flexqueue_games_past_3_weeks = flexqueue_games_this_season
+            .filter(gameref => gameref.game_start_time.getTime() > (new Date().getTime()-1000*60*60*24*this.time_limit_days));
 
-              if (this.queueing_for === GameType.SOLO_QUEUE) {
-                this.current_queue_past_3_weeks = soloqueue_games_past_3_weeks;
-              } else if (this.queueing_for === GameType.FLEX_QUEUE_5V5) {
-                this.current_queue_past_3_weeks = flexqueue_games_past_3_weeks;
-              } else {
-                // Unsupported queue type
-                this.errors.push("Unsupported queue type. (This shouldn't be possible. UI y u let this happen?)");
-                this.current_queue_past_3_weeks = [];
-              }
-            }
-          });
+          if (soloqueue_games_this_season.length > 0) {
+            this.soloqueue_games_this_season = soloqueue_games_this_season;
+          }
+          if (flexqueue_games_this_season.length > 0) {
+            this.flexqueue_games_this_season = flexqueue_games_this_season;
+          }
+          if (soloqueue_games_past_3_weeks.length > 0) {
+            this.soloqueue_games_past_3_weeks = soloqueue_games_past_3_weeks;
+          }
+          if (flexqueue_games_past_3_weeks.length > 0) {
+            this.flexqueue_games_past_3_weeks = flexqueue_games_past_3_weeks;
+          }
+
+          if (this.queueing_for === GameType.SOLO_QUEUE) {
+            this.current_queue_past_3_weeks = soloqueue_games_past_3_weeks;
+          } else if (this.queueing_for === GameType.FLEX_QUEUE_5V5) {
+            this.current_queue_past_3_weeks = flexqueue_games_past_3_weeks;
+          } else {
+            // Unsupported queue type
+            this.error_message = "Unsupported queue type. (This shouldn't be possible. UI y u let this happen?)";
+            this.current_queue_past_3_weeks = [];
+          }
+        } else if (api_res.type === ResType.ERROR) {
+          this.error_message = `An error happened. Try again later. (${api_res.error})`;
+        }
       });
+  }
+
+  ngOnInit() {
+    // Get first loads of static data (except items, saved for later / in sub-components)
+    this.metadata.champions$.first().subscribe(container => this.champions = container);
   }
 
 }
